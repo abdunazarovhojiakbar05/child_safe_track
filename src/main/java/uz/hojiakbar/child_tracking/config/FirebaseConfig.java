9a69fbedb81419195d4f4c948255fe665c47f96d
abdunazarovhojiakbar05 <abdunazarovhojiakbar05@gmail.com>
2026-06-03 20:27:40 +0500

    child_safe_track

--- src/main/java/uz/hojiakbar/child_tracking/config/FirebaseConfig.java
+++ src/main/java/uz/hojiakbar/child_tracking/config/FirebaseConfig.java
@@ -5,8 +5,11 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 
 @Configuration
@@ -16,9 +19,18 @@ public class FirebaseConfig {
     public FirebaseApp firebaseApp() {
         try {
             if (FirebaseApp.getApps().isEmpty()) {
-                FileInputStream serviceAccount = new FileInputStream(
-                        "src/main/resources/firebase-service-account.json"
-                );
+
+                String firebaseJson = System.getenv("FIREBASE_SERVICE_ACCOUNT");
+
+                InputStream serviceAccount;
+                if (firebaseJson != null && !firebaseJson.isEmpty()) {
+                     serviceAccount = new ByteArrayInputStream(firebaseJson.getBytes());
+                } else {
+                     serviceAccount = new FileInputStream(
+                            "src/main/resources/firebase-service-account.json"
+                    );
+                }
+
                 FirebaseOptions options = FirebaseOptions.builder()
                         .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                         .build();
